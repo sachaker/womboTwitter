7982a45dd93c479ab0dd96102f6730ee2b0b9b71
Sacha (McElligott) Schermerhorn <midgetman69@gmail.com>
2021-03-02 17:09:57 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+## Tweepy
+twitter_api_key = ''
+twitter_secret_key = ''
+twitter_access_token = ''
+twitter_access_token_secret = ''
+
+## Twilio
+twilio_from = '' # this is the number given to you by Twilio
+twilio_to = '' # your own phone number
+twilio_api_key = ''
+twilio_auth_token = ''
